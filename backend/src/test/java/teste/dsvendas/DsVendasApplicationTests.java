216241d9d8e0df64785f52d74dc3f5723bface0d
matheus-mag <matheusmag1103@gmail.com>
2021-09-08 16:56:42 -0300

    Project Created

---
+++ backend/src/test/java/teste/dsvendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
